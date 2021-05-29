69121a8681afbb0017192a433932e84bdceb332c
Pablo Rizo <prozasoconw4@gmail.com>
2021-05-29 14:16:16 -0500

    llego el fin de semana

--- api/asistencias/controllers/asistencias.js
+++ api/asistencias/controllers/asistencias.js
@@ -1,8 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    let body = ctx.request.body;
+
+    entity = await strapi.connections.default.raw(`
+        INSERT INTO asistencias (
+            AsistenciaFecha,
+            AsistenciaClase,
+            AsistenciaAlumno,
+            AsistenciaCheck
+        ) VALUES (
+            '${body.asistenciaFecha}',
+            ${body.asistenciaClase},
+            ${body.asistenciaAlumno},
+            ${body.asistenciaCheck}
+        );
+    `);
+
+    ctx.send(
+      {
+        message: "The content was created!",
+      },
+      200
+    );
+  },
+};
